ad810d0ebb2d98607ceec5fd8824de9f801fb0b2
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 11:09:38 +0000

    Mon Dec 23 11:09:38 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,8 +1,12 @@
 
 from solutions.SUM import sum_solution
+import sys
+
+sys.path.insert('~/iwoca/accelerate_runner/test/solutions/SUM')
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
 
