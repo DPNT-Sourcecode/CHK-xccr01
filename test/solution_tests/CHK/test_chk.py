e99f540c69bbef7ab9e1d792bec2e498a73e275d
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:11:41 +0000

    Mon Dec 23 12:11:41 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -5,10 +5,14 @@ import unittest
 class TestSum(unittest.TestCase):
     def test_checkout_sum (self):
         assert checkout_solution.checkout("ABCD") == 115
+
+    def test_checkout_sum_2 (self ):
+        assert checkout_solution.checkout("ABCDA") == 165
     
     def test_checkout_type(self):
         assert checkout_solution.checkout(True) == -1
 
     def test_checkout_bad_input(self):
         assert checkout_solution.checkout("AABZ") == -1
+
 
