88890ba787eb3f18ae3a333a3cfb4c60da461aca
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 11:15:38 +0000

    Mon Dec 23 11:15:38 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -8,5 +8,12 @@ class TestSum():
     def test_dif_sum(self):
         assert sum_solution.compute(5, 2) == 7
 
+    def test_decimal(self):
+        assert sum_solution.compute(1.3, 2.5) == 3
+
+    def test_type_checking(self):
+        assert sum_solution.comput
+
+
 
 
