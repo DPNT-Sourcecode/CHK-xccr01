92edc19c7c6cc67be7380c9d696070e0ee3ac2e2
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 11:39:38 +0000

    Mon Dec 23 11:39:38 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,16 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     if x < 0 or x > 100:
-        raise ValueError("number not within accepted range of 0 - 100")
+        raise ValueError("number x not within accepted range of 0 - 100")
     if y < 0 or y > 100:
-        raise ValueError("number not within accepted range 0 - 100")
-
+        raise ValueError("number y not within accepted range 0 - 100")
+    if type(x) is not int:
+        raise TypeError("x must be an integer")
+    if type(y) is not int:
+        raise TypeError("x must be an integer")
     return x + y
     
+
 
 
 
