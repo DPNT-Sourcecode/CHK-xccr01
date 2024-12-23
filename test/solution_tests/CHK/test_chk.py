8136e653668a3b23bee27964ef738426c04c6b11
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:17:41 +0000

    Mon Dec 23 12:17:41 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -8,11 +8,15 @@ class TestSum(unittest.TestCase):
 
     def test_checkout_sum_2 (self ):
         assert checkout_solution.checkout("ABCDA") == 165
+
+    def test_checkout_special_offer_B (self ):
+        assert checkout_solution.checkout("ABCDAB") == 130
     
     def test_checkout_type(self):
         assert checkout_solution.checkout(True) == -1
 
     def test_checkout_bad_input(self):
         assert checkout_solution.checkout("AABZ") == -1
+
 
 
