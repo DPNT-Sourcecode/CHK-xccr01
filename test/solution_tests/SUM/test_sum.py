de53ea7ab453e9a173fccee6eb109143f9773fa5
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 11:03:38 +0000

    Mon Dec 23 11:03:38 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,8 +1,8 @@
-import sys
-sys.path.append('test/solution_tests/SUM')
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
