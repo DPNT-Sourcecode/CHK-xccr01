0f4b344a01c0bf79372bc79bdbe7edf556bdccb5
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:14:41 +0000

    Mon Dec 23 12:14:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,7 +8,15 @@ def checkout(skus):
     B = 30
     C = 20
     D = 15
-
-    total = A + B + C + D
+    for item in skus.upper():
+        if item == "A":
+            total += 50
+        if item == "B":
+            total += 30
+        if item == "C":
+            total += 20
+        if item == "D":
+            total +=15
     return total
+
 
