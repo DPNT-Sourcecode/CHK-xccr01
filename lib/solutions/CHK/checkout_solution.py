131e5b7feff18cac03b741b6c94b9766708d2fe0
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:08:41 +0000

    Mon Dec 23 12:08:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+    A = 50
+    B = 30
+    C = 20
+    D = 15
+
+    total = A + B + C + D
+    return total
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,10 +4,11 @@ import unittest
 
 class TestSum(unittest.TestCase):
     def test_checkout_sum (self):
-        assert checkout_solution.checkout("AB") == 80
+        assert checkout_solution.checkout("ABCD") == 115
     
     def test_checkout_type(self):
         assert checkout_solution.checkout(True) == -1
 
     def test_checkout_bad_input(self):
         assert checkout_solution.checkout("AABZ") == -1
+
