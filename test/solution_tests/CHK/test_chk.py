8c87c3a32b75e785124e20984487040b1f71b74a
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 15:47:11 +0000

    Mon Dec 23 15:47:11 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,29 +6,29 @@ class TestSum(unittest.TestCase):
     def test_checkout_type(self):
         assert checkout_solution.checkout(True) == -1
 
-    # def test_checkout_bad_input(self):
-    #     assert checkout_solution.checkout("AABZ") == -1
+    def test_checkout_bad_input(self):
+        assert checkout_solution.checkout("AAB1") == -1
 
     def test_checkout_sum (self):
         assert checkout_solution.checkout("A") == 50
 
-    # def test_checkout_sum_2 (self ):
-    #     assert checkout_solution.checkout("ABCDA") == 165
+    def test_checkout_sum_2 (self ):
+        assert checkout_solution.checkout("ABCDA") == 165
 
-    # def test_checkout_special_offer_B (self ):
-    #     assert checkout_solution.checkout("ABCDAB") == 180
+    def test_checkout_special_offer_B (self ):
+        assert checkout_solution.checkout("ABCDAB") == 180
     
-    # def test_checkout_special_offer_A (self ):
-    #     assert checkout_solution.checkout("ABCDABAA") == 260
+    def test_checkout_special_offer_A (self ):
+        assert checkout_solution.checkout("ABCDABAA") == 260
     
-    # def test_checkout_special_offer_E (self ):
-    #     assert checkout_solution.checkout("ABCDABAAEEEFFF") == 385
+    def test_checkout_special_offer_E (self ):
+        assert checkout_solution.checkout("ABCDABAAEEEFFF") == 385
 
-    # def test_checkout_special_offer_E2 (self ):
-    #     assert checkout_solution.checkout("ABCDAABABAEEFF") == 380
+    def test_checkout_special_offer_E2 (self ):
+        assert checkout_solution.checkout("ABCDAABABAEEFF") == 380
 
-    # def test_checkout_A5(self ):
-    #     assert checkout_solution.checkout("") == 0
+    def test_checkout_A5(self ):
+        assert checkout_solution.checkout("") == 0
     
     def test_checkout_A6 (self ):
         assert checkout_solution.checkout("AAAAAA") == 250
