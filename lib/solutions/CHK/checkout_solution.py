7c4e3e2c2fa663cdebf9c2222026c3fb13d23e65
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:20:41 +0000

    Mon Dec 23 12:20:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,21 @@
-
+import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     total = 0
     A = 50
+    # "3A" = 130
     B = 30
+    "2B" = 45
     C = 20
     D = 15
+
+    formatted_skus = skus.upper()
+    A_list = re.findall("formatted_skus", "A")
+    B_list = re.findall("formatted_skus", "B")
+    C_list = re.findall("formatted_skus", "C")
+    D_list = re.findall("formatted_skus", "D")
     for item in skus.upper():
         if item == "A":
             total += 50
@@ -18,5 +26,6 @@ def checkout(skus):
         if item == "D":
             total +=15
     return total
+
 
 
