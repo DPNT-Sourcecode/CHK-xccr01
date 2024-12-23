e066e0775011c041f1272ae52a1389815ee05dca
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 12:41:41 +0000

    Mon Dec 23 12:41:41 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,19 +12,19 @@ def checkout(skus):
     D_cost = 15
 
     formatted_skus = skus.upper()
-    A_list = re.findall("A", "formatted_skus")
-    B_list = re.findall("B", "formatted_skus")
-    C_list = re.findall("C", "formatted_skus")
-    D_list = re.findall("D", "formatted_skus")
-    
-    # if item == "A":
-    #     total += 50
-    # calculate total cost of B including the special offers
-    # if len(B_list)%2 == 0:
-    #     B_total = len(B_list)/2 * B_deal
-    # else:
-    #     B_total = (len(B_list) - 1)/2 * B_deal + B_cost
+    A_list = re.findall("A", formatted_skus)
+    B_list = re.findall("B", formatted_skus)
+    C_list = re.findall("C", formatted_skus)
+    D_list = re.findall("D", formatted_skus)
 
-    total = len(A_list)#*A_cost + B_total + len(C_list)*C_cost + len(D_list)*D_cost
+    
+    # calculate total cost of B including the special offers
+    if len(B_list)%2 == 0:
+        B_total = len(B_list)/2 * B_deal
+    else:
+        B_total = (len(B_list) - 1)/2 * B_deal + B_cost
+
+    total = len(A_list)*A_cost + B_total + len(C_list)*C_cost + len(D_list)*D_cost
     return total
+
 
