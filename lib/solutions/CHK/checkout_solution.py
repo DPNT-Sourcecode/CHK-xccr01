567ff58ccebf6afd7a055bb6c5bef0a52743c208
sbx_user1051 <sbx_user1051@169.254.149.210>
2024-12-23 13:26:17 +0000

    Mon Dec 23 13:26:17 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,7 +5,7 @@ import re
 def checkout(skus):
     if type(skus) is not str or skus.islower():
         return -1
-    if re.search("[^ABCD]", skus) :
+    if re.search("[^ABCDE]", skus) :
         return -1
     total = 0
     A_cost = 50
@@ -15,12 +15,14 @@ def checkout(skus):
     B_deal = 45
     C_cost = 20
     D_cost = 15
+    E_cost = 40
 
     skus = skus.upper()
     A_list = re.findall("A", skus)
     B_list = re.findall("B", skus)
     C_list = re.findall("C", skus)
     D_list = re.findall("D", skus)
+    E_list = re.findall("E", skus)
 
     # calculate total cost of A including the special offers
     A_items = len(A_list)
@@ -29,7 +31,7 @@ def checkout(skus):
     elif A_items == 2:
         A_total = A_cost * 2
     elif A_items%5 == 0:
-        A-total = A_items/5 * A_deal_5
+        A_total = A_items/5 * A_deal_5
     elif A_items%3 == 0:
         A_total = A_items/3 * A_deal_3
     elif A_items == 4:
@@ -37,16 +39,25 @@ def checkout(skus):
     else:
         A_total = ((A_items - A_items%5)/5 * A_deal_5) +(A_items%5 * A_cost)
 
-
-    # calculate total cost of B including the special offers
     B_items = len(B_list)
+    E_items = len(E_list)
+    #calculate effect of E on B
+    if E_items >= 2:
+        E_items//2
+        B_items = B_items - E_items//2
+    # calculate total cost of B including the special offers
     if B_items%2 == 0:
         B_total = B_items/2 * B_deal
     else:
         B_total = (B_items - 1)/2 * B_deal + B_cost
 
+    
+    
+
+
     total = A_total + B_total + len(C_list)*C_cost + len(D_list)*D_cost
     return total
+
 
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,6 +3,12 @@ from solutions.CHK import checkout_solution
 import unittest
 
 class TestSum(unittest.TestCase):
+    def test_checkout_type(self):
+        assert checkout_solution.checkout(True) == -1
+
+    def test_checkout_bad_input(self):
+        assert checkout_solution.checkout("AABZ") == -1
+
     def test_checkout_sum (self):
         assert checkout_solution.checkout("A") == 50
 
@@ -15,9 +21,7 @@ class TestSum(unittest.TestCase):
     def test_checkout_special_offer_A (self ):
         assert checkout_solution.checkout("ABCDABAA") == 260
     
-    def test_checkout_type(self):
-        assert checkout_solution.checkout(True) == -1
+    def test_checkout_special_offer_E (self ):
+        assert checkout_solution.checkout("ABCDABAAEEE") == 245
 
-    def test_checkout_bad_input(self):
-        assert checkout_solution.checkout("AABZ") == -1
 
